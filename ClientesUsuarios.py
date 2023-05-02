71fe6265abecb262f9b0f64c2726fa679582017a
Cristian Loera <cristian@loera>
2023-05-02 16:59:48 -0600

    Se agrego FINDARFP a la lista de Clientes

--- ClientesUsuarios.py
+++ ClientesUsuarios.py
@@ -3,16 +3,14 @@ import funcion_preguntas
 
 '''Clientes y Usuarios'''
 clientes = { "Clientes y usuarios" : ["SOFIPO", "SOFICO", "SOCAP", "OIF", "Fondo de pago Electronico","Banca Multiple","Banca Desarrollo","Casa de Bolsa"],
-             "Clientes" : ["Sociedad Anonima de Fondo de Inversion", "Asesor de inversion", "Almacen General de Deposito", "SOFOM", "Union de Credito", "Financiamiento Colectivo"],
+             "Clientes" : ["Sociedad Anonima de Fondo de Inversion", "Asesor de inversion", "Almacen General de Deposito", "SOFOM", "Union de Credito", "Financiamiento Colectivo", "FINDARFP"],
              "Usuarios" : ["Centro Cambiario", "Transmisor de dinero", "Casa de Cambio"]}
 
-def clientesyusuarios():        
-    
+def clientesyusuarios():
+
 
     tema = "Clientes y Usuarios"
     pregunta = "Que SOB tienen "
-        
+
     calificacion = funcion_preguntas.preguntas_list(clientes, tema, pregunta)
     return calificacion
-
-
